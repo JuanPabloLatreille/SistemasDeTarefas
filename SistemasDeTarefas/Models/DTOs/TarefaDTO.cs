ace016bd6ac6179afbe2fe22ffc4a921d701bdae
Juan Pablo <juanpablolatreille2015@outlook.com>
2023-07-10 00:15:04 -0300

    Adicionando annotations no DTO das tarefas

--- SistemasDeTarefas/Models/DTOs/TarefaDTO.cs
+++ SistemasDeTarefas/Models/DTOs/TarefaDTO.cs
@@ -1,11 +1,17 @@
 ﻿using SistemasDeTarefas.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemasDeTarefas.Models.DTOs
 {
     public class TarefaDTO
     {
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(1000)]
         public string Description { get; set; }
+        [Required]
         public StatusTarefa Status { get; set; }
         public int? UsuarioId { get; set; }
     }
